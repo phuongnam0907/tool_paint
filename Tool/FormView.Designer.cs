033a8c70a7e8fd663948051120349f6fb8cca3b9
Le Phuong Nam <phuongnam0907@gmail.com>
2020-11-11 11:05:06 +0700

    First Init
    Signed-off-by: Le Phuong Nam <phuongnam0907@gmail.com>

---
+++ Tool/FormView.Designer.cs
@@ -0,0 +1,145 @@
+﻿namespace Tool
+{
+    partial class FormView
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox = new System.Windows.Forms.PictureBox();
+            this.groupBoxSetting = new System.Windows.Forms.GroupBox();
+            this.groupBoxDrawing = new System.Windows.Forms.GroupBox();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).BeginInit();
+            this.groupBoxDrawing.SuspendLayout();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pictureBox
+            // 
+            this.pictureBox.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pictureBox.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.pictureBox.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox.Name = "pictureBox";
+            this.pictureBox.Size = new System.Drawing.Size(734, 532);
+            this.pictureBox.TabIndex = 0;
+            this.pictureBox.TabStop = false;
+            this.pictureBox.Paint += new System.Windows.Forms.PaintEventHandler(this.pictureBox_Paint);
+            // 
+            // groupBoxSetting
+            // 
+            this.groupBoxSetting.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBoxSetting.AutoSize = true;
+            this.groupBoxSetting.Location = new System.Drawing.Point(752, 12);
+            this.groupBoxSetting.Name = "groupBoxSetting";
+            this.groupBoxSetting.Size = new System.Drawing.Size(280, 264);
+            this.groupBoxSetting.TabIndex = 1;
+            this.groupBoxSetting.TabStop = false;
+            this.groupBoxSetting.Text = "Thiết Lập";
+            // 
+            // groupBoxDrawing
+            // 
+            this.groupBoxDrawing.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBoxDrawing.AutoSize = true;
+            this.groupBoxDrawing.Controls.Add(this.tableLayoutPanel1);
+            this.groupBoxDrawing.Location = new System.Drawing.Point(749, 276);
+            this.groupBoxDrawing.Name = "groupBoxDrawing";
+            this.groupBoxDrawing.Size = new System.Drawing.Size(283, 268);
+            this.groupBoxDrawing.TabIndex = 2;
+            this.groupBoxDrawing.TabStop = false;
+            this.groupBoxDrawing.Text = "Vẽ";
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tableLayoutPanel1.AutoSize = true;
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Controls.Add(this.button1, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.textBox1, 1, 0);
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(6, 19);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 2;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25.83333F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 74.16666F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(267, 240);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(3, 3);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(127, 56);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.buttonDraw_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(136, 3);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(128, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // FormView
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1044, 556);
+            this.Controls.Add(this.groupBoxDrawing);
+            this.Controls.Add(this.groupBoxSetting);
+            this.Controls.Add(this.pictureBox);
+            this.Name = "FormView";
+            this.Text = "Lotus";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).EndInit();
+            this.groupBoxDrawing.ResumeLayout(false);
+            this.groupBoxDrawing.PerformLayout();
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox;
+        private System.Windows.Forms.GroupBox groupBoxSetting;
+        private System.Windows.Forms.GroupBox groupBoxDrawing;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ Tool/FormView.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Tool
+{
+    public partial class FormView : Form
+    {
+        private struct Coordinates
+        {
+            public Coordinates(int x, int y, int z)
+            {
+                X = x;
+                Y = y;
+                Z = z;
+            }
+
+            public int X { get; }
+            public int Y { get; }
+            public int Z { get; }
+
+            public override string ToString() => $"({X}, {Y}, {Z})";
+        }
+
+        private int _x;
+        private int _y;
+        List<Coordinates> list = new List<Coordinates>();
+        Bitmap bmp;
+        Graphics g;
+
+        public FormView()
+        {
+            InitializeComponent();
+
+            _x = pictureBox.Width / 2;
+            _y = pictureBox.Height / 2;
+        }
+
+        private void pictureBox_Paint(object sender, PaintEventArgs e)
+        {
+            bmp = new Bitmap(pictureBox.Width, pictureBox.Height);
+            using (g = Graphics.FromImage(bmp))
+            {
+                int cX = _x;
+                int cY = _y;
+                int tX = 0;
+                int tY = 0;
+                list.Add(new Coordinates(_x, _y, 0));
+                foreach (Coordinates element in list)
+                {
+                    tX = cX;
+                    tY = cY;
+                    cX = element.X;
+                    cY = element.Y;
+                    g.DrawLine(new Pen(Color.Red), tX, tY, cX, cY);
+                }
+                g.FillRectangle(Brushes.BlueViolet, _x, _y, 10, 10);
+
+            }
+            pictureBox.Image = bmp;
+        }
+
+        private void buttonDraw_Click(object sender, EventArgs e)
+        {
+            _x += int.Parse(textBox1.Text);
+            _y += 2;
+            list.Add(new Coordinates(_x, _y, 0));
+            Invalidate();
+        }
+    }
+}
